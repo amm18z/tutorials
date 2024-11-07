4aa6947dd1c80cfa81ea569f269a487d6dd439bc
Aidan M <am@bizylife.com>
2024-11-07 19:14:59 +0000

    Chapter 6 in Odoo server framework 101

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,19 +1,25 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+
 
 class EstateProperty(models.Model):
 	_name = "estate.property"
 	_description = "A building or buildings and the land belonging to it or them."
 
-	name = fields.Char(string='name', required=True)
-	description = fields.Text(string='description')
-	postcode = fields.Char(string='[postcode')
-	date_availability = fields.Date(string='date availability')
-	expeceted_price = fields.Float(string='expeceted price', required=True)
-	selling_price = fields.Float(string='selling price')
-	bedrooms = fields.Integer(string='bedrooms')
-	living_area = fields.Integer(string='living area')
-	facades = fields.Integer(string='facades')
-	garage = fields.Boolean(string='garage')
-	garden = fields.Boolean(string='garden')
-	garden_area = fields.Integer(string='garden area')
-	garden_orientation = fields.Selection(string='orientation', selection = [('north','North'),('south','South'),('east','East'),('west','West')])
+
+	name = fields.Char(string='Title', required=True)
+	description = fields.Text(string='Description')
+	postcode = fields.Char(string='Postcode')
+	date_availability = fields.Date(string='Available From', copy=False, default=fields.Date.add(fields.Date.today(),months=3)) #took me forever to figure out it's just months=3 for the 2nd argument GRRR!!
+	expected_price = fields.Float(string='Expected Price', required=True)
+	selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+	bedrooms = fields.Integer(string='Bedrooms', default=2)
+	living_area = fields.Integer(string='Living Area (sqm)')
+	facades = fields.Integer(string='Facades')
+	garage = fields.Boolean(string='Garage')
+	garden = fields.Boolean(string='Garden')
+	garden_area = fields.Integer(string='Garden Area (sqm)')
+	garden_orientation = fields.Selection(string='Garden Orientation', selection = [('north','North'),('south','South'),('east','East'),('west','West')])
+	state = fields.Selection(string='Status', selection = [('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')], copy=False, default='new')
+	active = fields.Boolean(default=True)
+	
