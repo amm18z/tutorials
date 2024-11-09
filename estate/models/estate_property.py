0f372f41277047172c94d5d6922793b25d849796
Aidan M <am@bizylife.com>
2024-11-09 04:52:49 +0000

    Chapter 8 in Odoo server framerwork 101 tutorial series

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstateProperty(models.Model):
 	_name = "estate.property"
@@ -28,3 +28,38 @@ class EstateProperty(models.Model):
 	tag_ids = fields.Many2many('estate.property.tag', string="Property Tag")
 
 	offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Property Offer')
+
+	total_area = fields.Integer(compute="_compute_total_area")
+
+	@api.depends("living_area", "garden_area")	# Lets the ORM layer know the fields upon which on the computed field depends, so that 
+	def _compute_total_area(self):				# the computed field can be recomputed if/when its dependencies change in value
+		for record in self:
+			record.total_area = record.living_area + record.garden_area
+
+	''' 'self' is a collection.
+		The object self is a recordset, i.e. an ordered collection of records. 
+		It supports the standard Python operations on collections, e.g. len(self) and iter(self), plus extra set operations such as recs1 | recs2.
+		Iterating over self gives the records one by one, where each record is itself a collection of size 1. 
+		You can access/assign fields on single records by using the dot notation, e.g. record.name. '''
+
+	best_price = fields.Float(compute="_compute_best_price", string="Best Offer")
+
+	# For relational fields it's possible to use paths through a field as a dependency
+	@api.depends("offer_ids.price")
+	def _compute_best_price(self):
+		temp_best_price = 0.0
+		for record in self:
+			if len(record.mapped('offer_ids.price')) > 0:
+				record.best_price = max(record.mapped('offer_ids.price')) # mapped() documentation: https://www.odoo.com/documentation/18.0/developer/reference/backend/orm.html#map
+			else:
+				record.best_price = 0
+
+
+	@api.onchange("garden")
+	def _onchange_garden(self):
+		if self.garden == True:
+			self.garden_area = 10
+			self.garden_orientation = 'north'
+		else:
+			self.garden_area = 0
+			self.garden_orientation = ''
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -9,9 +9,23 @@ class EstatePropertyOffer(models.Model):
     partner_id = fields.Many2one("res.partner", string="Buyer",required=True)
     property_id = fields.Many2one("estate.property",string="Property",required=True)
 
-    
-
-	
+    validity = fields.Integer(compute='_inverse_date_deadline', inverse='_compute_date_deadline', string='Validity', default='7', store=True)
 
 
+    date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_inverse_date_deadline', string="Deadline", store=True, default=fields.Date.add(fields.Date.today(),days=7))
 
+    @api.depends('create_date', 'date_deadline', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date != False:
+                record.date_deadline = fields.Date.add(fields.Date.to_date(record.create_date),days=record.validity)
+            else:
+                record.date_deadline = fields.Date.add(fields.Date.today(), days=record.validity)
+            
+    @api.depends('create_date', 'validity', 'date_deadline')
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date != False:
+                record.validity = (record.date_deadline - fields.Date.to_date(record.create_date)).days
+            else:
+                record.validity = (record.date_deadline - fields.Date.today()).days
